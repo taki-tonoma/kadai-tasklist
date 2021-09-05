0cc2f50b7864a0aa2559d0af536b8c6e81de6c5f
taki-tonoma <tonoma@i.softbank.jp>
2021-09-05 00:42:52 +0000

    task-list commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
 
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :show, :edit]
 
   def index
-    @tasks = Task.all
+    if logged_in?
+       @user = current_user
+       @task = current_user.tasks.build
+       @tasks = current_user.tasks.order('created_at desc').page(params[:page])
+    end
   end
 
   def show
@@ -14,14 +20,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
+        
     if @task.save
-      flash[:success] = 'Task が正常に登録されました'
-      redirect_to @task
+        flash[:success] = 'Taskが正常に登録されました'
+        redirect_to @task
     else
-      flash.now[:danger] = 'Task が登録されませんでした'
-      render :new
+        flash.now[:danger] = 'Taskが登録されませんでした'
+        render :new
     end
   end
 
@@ -56,4 +62,14 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def task_params
+      params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+        redirect_to root_url
+    end
+  end
 end
